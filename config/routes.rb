2058e3ae3ad0e4132f8ef06454df2fa35e375bf7
geirgie25 <willthomaspierce@yahoo.com>
2021-02-28 11:55:09 -0600

    S1: syntax error fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   # get 'authorized', to: 'sessions#page_requires_login'
-
-  # get 'sessions/new'
-  # get 'sessions/create'
-  # get 'sessions/login'
-  # get 'sessions/welcome'
-  # get 'accounts/new'
-  # get 'accounts/create'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
