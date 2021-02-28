361c1297180295454e257c388636f24beefbd035
geirgie25 <willthomaspierce@yahoo.com>
2021-02-28 12:04:45 -0600

    S1: HTML comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  # LOGIN INFORMATION
   resources :accounts, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   # get 'authorized', to: 'sessions#page_requires_login'
-  
+
 end
