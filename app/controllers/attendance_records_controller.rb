493c8d32cc27a3a151612e4acec1e2930eaf30ae
Jesse Sprinkle <jake.toto14@gmail.com>
2021-04-07 13:36:57 -0500

    small testing

--- app/controllers/attendance_records_controller.rb
+++ app/controllers/attendance_records_controller.rb
@@ -2,9 +2,17 @@
 
 # controls attendance records. temp
 class AttendanceRecordsController < ApplicationController
+  skip_before_action :user_authorized, only: %i[index]
   skip_before_action :admin_authorized, only: %i[index]
 
+  def filtered_records
+    @records = AttendanceRecord.in_committee(params[:committee_id])
+  end
+
   def index
-    @records = AttendanceRecord.records(current_user)
+    @records = AttendanceRecord.all
+    @records = @records.user(params[:user_id]) if params[:user_id]
+    @records = @records.in_committee(params[:committee_id]) if params[:committee_id]
+    puts @records
   end
 end
